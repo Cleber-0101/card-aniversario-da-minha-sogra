329acca073458f2740712fcaee7c9c6f6e2da8f1
Cleber-esx <cleber.batista@esx.com.br>
2023-12-18 18:00:38 -0300

    Projeto feliz aniversario sogra

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-  const birthdayCard = document.getElementById('birthdayCard');
+const birthdayCard = document.getElementById('birthdayCard');
 
-  birthdayCard.addEventListener('click', function () {
-    this.classList.toggle('open');
-  });
+  birthdayCard.addEventListener('click', toggleCard);
+  birthdayCard.addEventListener('touchstart', toggleCard); // Handle touch events
+
+  function toggleCard() {
+    birthdayCard.classList.toggle('opened');
+  }
+
+  // Open the card initially
+  toggleCard();
+
+  // Set a timeout to close the card after a certain duration (adjust as needed)
+  setTimeout(() => {
+    toggleCard();
+  }, 5000);
